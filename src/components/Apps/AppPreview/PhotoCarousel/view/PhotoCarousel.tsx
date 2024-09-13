e4159a392d79fd674e9b441ddfea152580cef5de
Hans Poreda <hansporeda@icloud.com>
2024-09-13 17:30:56 +0200

    unnecesarry import delted

--- src/components/Apps/AppPreview/PhotoCarousel/view/PhotoCarousel.tsx
+++ src/components/Apps/AppPreview/PhotoCarousel/view/PhotoCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./PhotoCarousel.css";
 
 import { Card, CardContent } from "../../../../ui/card";
