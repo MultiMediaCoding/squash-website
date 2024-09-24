8165b1f9188e9c02c6472717338a0932790abe0e
Hans Poreda <hansporeda@icloud.com>
2024-09-24 20:59:48 +0200

    Download Badges in mobile mode

---
+++ src/components/Apps/AppPreview/AppHeader/view/ComingSoonButton/ComingSoonButton.tsx
@@ -0,0 +1,9 @@
+import "./ComingSoonButton.css";
+
+export function ComingSoonButton() {
+  return (
+    <div className="coming-soon-button">
+      <div className="text-lg font-regular">Comming Soon</div>
+    </div>
+  );
+}
--- src/components/Apps/AppPreview/AppHeader/view/CommingSoonButton/CommingSoonButton.tsx
+++
@@ -1,9 +0,0 @@
-import "./CommingSoonButton.css";
-
-export function CommingSoonButton() {
-  return (
-    <div className="comming-soon-button">
-      <div className="text-lg font-regular">Comming Soon</div>
-    </div>
-  );
-}
